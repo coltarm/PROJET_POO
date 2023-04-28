5fb3fd94f10ad6c85bfb0bfe9f0dd638a9a21eba
coltarm <113551889+coltarm@users.noreply.github.com>
2023-04-28 12:10:01 +0200

    ajout de class event

---
+++ Gestion_alarme/src/CapteurListener.java
@@ -0,0 +1,5 @@
+
+public interface CapteurListener {
+	public void Nouveau_Concert( concert);
+
+}
---
+++ Gestion_alarme/src/ProblemeEvent.java
@@ -0,0 +1,9 @@
+import java.util.EventObject;
+
+public class ProblemeEvent extends EventObject{
+	
+	public ProblemeEvent(Object source) {
+		super(source);
+	}
+
+}
